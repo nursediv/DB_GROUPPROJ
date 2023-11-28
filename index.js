c443397511e3b15c55464a813427674a7fd157fc
Divya <129140048+nursediv@users.noreply.github.com>
2023-11-27 23:53:55 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,33 @@
-// Import necessary modules
 const express = require('express');
 const oracledb = require('oracledb');
-const dbConfig = require('./dbconfig'); 
-
-// Initialize the Express app
+const dbConfig = require('./dbconfig');
 const app = express();
-const port = 3001; 
+const port = 3001;
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 
-// API endpoint for the button action
+// Define a route
 app.get('/api/button-action', async (req, res) => {
-    let connection;
-
-    try {
-        // Establish a connection to the Oracle database
-        connection = await oracledb.getConnection(dbConfig);
-
-        // Example query - modify according to your database schema
-        const result = await connection.execute(`SELECT * FROM your_table`);
-
-        // Send the query results as a response
-        res.json(result.rows);
-    } catch (err) {
-        // Handle errors
+  let connection;
+  try {
+    connection = await oracledb.getConnection(dbConfig);
+    // Perform your SQL queries here
+    const result = await connection.execute(`SELECT * FROM your_table`);
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error occurred");
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
         console.error(err);
-        res.status(500).send("Error occurred while accessing the database.");
-    } finally {
-        // Close the database connection
-        if (connection) {
-            try {
-                await connection.close();
-            } catch (err) {
-                console.error(err);
-            }
-        }
+      }
     }
+  }
 });
 
-// Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
-
